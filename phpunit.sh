df5a4ca2934f93d4c54a4c49439710c38915323c
Miroslav Cillik <miro@keboola.com>
2016-03-07 16:46:12 +0100

    fix: composer install in phpunit.sh

--- phpunit.sh
+++ phpunit.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # install dependencies
+curl -sS https://getcomposer.org/installer | php
+echo "memory_limit = -1" >> /etc/php.ini
 php composer.phar install -n;
 
 # run test suite
