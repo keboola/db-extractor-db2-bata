eec2336ce2d1c1c025315d37e5067e879578f1dc
Ondra Jodas <ondrej.jodas@keboola.com>
2022-05-10 15:57:52 +0200

    migrate to GH Actions

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/db-extractor-db2-bata quay.io/keboola/db-extractor-db2-bata:$TRAVIS_TAG
-docker images
-docker push quay.io/keboola/db-extractor-db2-bata:$TRAVIS_TAG
