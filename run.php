eb8713b2e461e62ea6a835410292fc8f2f22bf6f
Ondra <ondra.jodas@gmail.com>
2022-12-14 23:53:56 +0100

    using config.json instead config.yml

--- run.php
+++ run.php
@@ -17,7 +17,7 @@ try {
         throw new UserException('Data folder not set.');
     }
 
-    $config = Yaml::parse(file_get_contents($arguments["data"] . "/config.yml"));
+    $config = json_decode(file_get_contents($arguments["data"] . "/config.json"), true);
     $config['parameters']['data_dir'] = $arguments['data'];
     $config['parameters']['extractor_class'] = 'DB2';
 
